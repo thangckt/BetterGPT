63052a2dca39d8528f35345de8f603d07826836c
C. Thang Nguyen <46436648+thangckt@users.noreply.github.com>
2024-04-22 15:05:02 +0900

    u

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -2,7 +2,7 @@ export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
 const customAPIEndpoint =
   import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://chatgpt-api.shn.hk/v1/';
 
-const localhostAPIEndpoint = 'http://127.0.0.1:8080/v1/chat/completions'
+const localhostAPIEndpoint = 'http://localhost:8080/v1/chat/completions'
 
 export const defaultAPIEndpoint =
   import.meta.env.VITE_DEFAULT_API_ENDPOINT || localhostAPIEndpoint;
